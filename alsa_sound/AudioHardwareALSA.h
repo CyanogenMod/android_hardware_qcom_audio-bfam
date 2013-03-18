da370fa7de2883d4241b6a9282ea99860adc0933
Shiv Maliyappanahalli <smaliyap@codeaurora.org>
2013-03-18 15:36:35 -0700

    alsa_sound: Change SGLTE to Voice2 for reusability of code
    Replace SGLTE with Voice2 for reusability of code.
    
    Change-Id: Ic4755f848c582e358759cc7d56d11100523cc121

--- alsa_sound/AudioHardwareALSA.h
+++ alsa_sound/AudioHardwareALSA.h
@@ -298,8 +298,8 @@ public:
     void     setVocRecMode(uint8_t mode);
     void     setVoLTEMicMute(int state);
     void     setVoLTEVolume(int vol);
-    void     setSGLTEMicMute(int state);
-    void     setSGLTEVolume(int vol);
+    void     setVoice2MicMute(int state);
+    void     setVoice2Volume(int vol);
     status_t setEcrxDevice(char *device);
     void     setInChannels(int);
     //TODO:check if this needs to be public
@@ -916,7 +916,7 @@ protected:
     void                enableVoiceCall(char* verb, char* modifier, int mode, int device);
     bool                routeVoiceCall(int device, int  newMode);
     bool                routeVoLTECall(int device, int newMode);
-    bool                routeSGLTECall(int device, int newMode);
+    bool                routeVoice2Call(int device, int newMode);
     friend class AudioSessionOutALSA;
     friend class AudioStreamOutALSA;
     friend class AudioStreamInALSA;
@@ -948,7 +948,7 @@ protected:
     bool                mMicMute;
     int mCSCallActive;
     int mVolteCallActive;
-    int mSGLTECallActive;
+    int mVoice2CallActive;
     int mCallState;
     int mIsFmActive;
     bool mBluetoothVGS;
