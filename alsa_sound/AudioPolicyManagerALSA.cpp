f0c34e404e561758caaf991c962960f0390779ee
Karthik Reddy Katta <akkatta@codeaurora.org>
2013-03-22 19:04:07 +0530

    alsa_sound : Fix for audio routing speaker though BT is connected
    - Speaker device is selected by getDeviceForStrategy() though BT is
      connected as mA2dpSuspended is still remaining as 'true' even
      after ending the voice call.
    - This flag is not updated by checkA2dpSuspend() as force usage
      for communication and record is still FORCE_BT_SCO after ending
      voice call.
    - Modified condition check to set mA2dpSuspended to 'false' as soon
      as phone state is changed to MODE_NORMAL
    
    Change-Id: Ibc0d19b361755d8715c446401d2e14d914fd7020
    CRs-Fixed: 464404

--- alsa_sound/AudioPolicyManagerALSA.cpp
+++ alsa_sound/AudioPolicyManagerALSA.cpp
@@ -1809,7 +1809,7 @@ void AudioPolicyManager::checkA2dpSuspend()
     if (mA2dpSuspended) {
         if (((mScoDeviceAddress == "") ||
              ((mForceUse[AudioSystem::FOR_COMMUNICATION] != AudioSystem::FORCE_BT_SCO) &&
-              (mForceUse[AudioSystem::FOR_RECORD] != AudioSystem::FORCE_BT_SCO))) &&
+              (mForceUse[AudioSystem::FOR_RECORD] != AudioSystem::FORCE_BT_SCO))) ||
              ((mPhoneState != AudioSystem::MODE_IN_CALL) &&
               (mPhoneState != AudioSystem::MODE_RINGTONE))) {
 
@@ -1818,7 +1818,7 @@ void AudioPolicyManager::checkA2dpSuspend()
     } else {
         if (((mScoDeviceAddress != "") &&
              ((mForceUse[AudioSystem::FOR_COMMUNICATION] == AudioSystem::FORCE_BT_SCO) ||
-              (mForceUse[AudioSystem::FOR_RECORD] == AudioSystem::FORCE_BT_SCO))) ||
+              (mForceUse[AudioSystem::FOR_RECORD] == AudioSystem::FORCE_BT_SCO))) &&
              ((mPhoneState == AudioSystem::MODE_IN_CALL) ||
               (mPhoneState == AudioSystem::MODE_RINGTONE))) {
 
