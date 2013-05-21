da97f8d1a36219a050ee1af6e2d07e008109755a
Amit Shekhar <ashekhar@codeaurora.org>
2013-05-21 12:15:50 -0700

    alsa_sound: added check for 0 bytesWritten before sleep
    -If the write to BT stream fails then the write thread should sleep and
     let other thread (eg: stopA2DP) acquire lock to prevent a deadlock.
    -Writing to BT Stream can potentially return zero which may causes
     deadlock. Therefore, adding check for zero as well.
    
    Change-Id: I9864756b00d752df2bbaf2b56b1ec3a45f290249
    CRs-Fixed: 492991

--- alsa_sound/AudioHardwareALSA.cpp
+++ alsa_sound/AudioHardwareALSA.cpp
@@ -3089,7 +3089,7 @@ void AudioHardwareALSA::extOutThreadFunc() {
             }
             //If the write fails make this thread sleep and let other
             //thread (eg: stopA2DP) to acquire lock to prevent a deadlock.
-            if(bytesWritten == -1) {
+            if(bytesWritten == -1 || bytesWritten == 0) {
                 ALOGV("bytesWritten = %d",bytesWritten);
                 usleep(10000);
                 break;
